a2ef6495cffe3a89ad167e2bc8761f97d62ed4d9
Manuel Quartino <orbi.net.uy@gmail.com>
2026-04-20 18:26:58 -0300

    feat: use custom favicon.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "WaveTransport",
   description: "Plataforma de reserva y gestión de viajes",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
